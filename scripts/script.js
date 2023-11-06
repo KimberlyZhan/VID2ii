a3cc2c333156c22c286d3cba90e3ee7c8f04526a
KimberlyZhan <112623368+KimberlyZhan@users.noreply.github.com>
2023-11-06 11:28:20 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,10 @@
 // JavaScript Document
-console.log("Howdy!");
+console.log("Hi Barbie! Hi Ken!");
+
+///////// KLEDINGKAST OUTFITS /////////
+var debutOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(1)");
+var chirurgOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(2)");
+var zwemmerOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(3)");
+var presidentOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(4)");
+var schermerOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(5)");
+var pandemieOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(6)");
