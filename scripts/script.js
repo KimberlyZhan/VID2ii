629193b7476297033f36017cbbe133f0eeb65368
KimberlyZhan <112623368+KimberlyZhan@users.noreply.github.com>
2023-11-07 11:44:12 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,18 +2,12 @@ console.log("Hi Barbie! Hi Ken!");
 console.log(poppenButton);
 
 //---------------- KLEDINGKAST OUTFITS ----------------//
-var debutOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(1)");
-var chirurgOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(2)");
-var zwemmerOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(3)");
-var presidentOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(4)");
-var schermerOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(5)");
-var pandemieOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(6)");
-
-//-------------- ROZE KAST MET POPPENHUIS --------------//
-var poppenButton = document.querySelectorAll("section:nth-of-type(3) button")
-
-// ------ informatie over barbie poppen
-var tekstElement = document.querySelector("section:nth-of-type(4) p")
+var debutOutfit = document.querySelector("ul:first-of-type button:nth-of-type(1)");
+var chirurgOutfit = document.querySelector("ul:first-of-type button:nth-of-type(2)");
+var zwemmerOutfit = document.querySelector("ul:first-of-type button:nth-of-type(3)");
+var presidentOutfit = document.querySelector("ul:first-of-type button:nth-of-type(4)");
+var schermerOutfit = document.querySelector("ul:first-of-type button:nth-of-type(5)");
+var pandemieOutfit = document.querySelector("ul:first-of-type button:nth-of-type(6)");
 
 
 
