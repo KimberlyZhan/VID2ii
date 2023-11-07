c9528e2783fe8754095b3cf664b927252599058e
KimberlyZhan <112623368+KimberlyZhan@users.noreply.github.com>
2023-11-07 13:10:58 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,12 @@ console.log("Hi Barbie! Hi Ken!");
 console.log(poppenButton);
 
 //---------------- KLEDINGKAST OUTFITS ----------------//
-var debutOutfit = document.querySelector("ul:first-of-type button:nth-of-type(1)");
-var chirurgOutfit = document.querySelector("ul:first-of-type button:nth-of-type(2)");
-var zwemmerOutfit = document.querySelector("ul:first-of-type button:nth-of-type(3)");
-var presidentOutfit = document.querySelector("ul:first-of-type button:nth-of-type(4)");
-var schermerOutfit = document.querySelector("ul:first-of-type button:nth-of-type(5)");
-var pandemieOutfit = document.querySelector("ul:first-of-type button:nth-of-type(6)");
+var debutOutfit = document.querySelector("ul button:nth-of-type(1)");
+var chirurgOutfit = document.querySelector("ul button:nth-of-type(2)");
+var zwemmerOutfit = document.querySelector("ul button:nth-of-type(3)");
+var presidentOutfit = document.querySelector("ul button:nth-of-type(4)");
+var schermerOutfit = document.querySelector("ul button:nth-of-type(5)");
+var pandemieOutfit = document.querySelector("ul button:nth-of-type(6)");
 
 
 
