6458867bb43c457755e9aaacbac33b737ba43f62
KimberlyZhan <112623368+KimberlyZhan@users.noreply.github.com>
2023-11-07 10:10:11 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,32 @@
-// JavaScript Document
 console.log("Hi Barbie! Hi Ken!");
+console.log(poppenButton);
 
-///////// KLEDINGKAST OUTFITS /////////
+//---------------- KLEDINGKAST OUTFITS ----------------//
 var debutOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(1)");
 var chirurgOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(2)");
 var zwemmerOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(3)");
 var presidentOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(4)");
 var schermerOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(5)");
 var pandemieOutfit = document.querySelector("section:nth-of-type(2) button:nth-of-type(6)");
+
+//-------------- ROZE KAST MET POPPENHUIS --------------//
+var poppenButton = document.querySelectorAll("section:nth-of-type(3) button")
+
+// ------ informatie over barbie poppen
+var tekstElement = document.querySelector("section:nth-of-type(4) p")
+
+
+
+// function popInfo() {
+//     poppenButton[0].classList.add("hidden")
+// }
+
+// poppenButton[0].addEventListener("click", popInfo());
+
+// poppenButton[0].onclick = popInfo
+
+
+// function popInfo() {
+//     poppenButton[0].classList.toggle("informatieOverMij")
+// }
+
